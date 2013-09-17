55560da4847ec3c4472c2b19308c544f96b64597
Bradley Friemel <friemelb@gmail.com>
2013-09-16 20:23:50 -0500

    Resized Imgs

--- php/galary.php
+++ php/galary.php
@@ -9,92 +9,76 @@
 		<div>
 			<ul class="hoverbox">
 			<li>
-				<a href="#"><img src="resources/pic/photo01.jpg" alt="description" />
-				<img src="resources/pic/photo01.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/1.jpg" alt="description" />
+				<img src="resources/pic/1.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo02.jpg" alt="description" />
-				<img src="resources/pic/photo02.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/2.jpg" alt="description" />
+				<img src="resources/pic/2.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo03.jpg" alt="description" />
-				<img src="resources/pic/photo03.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/3.jpg" alt="description" />
+				<img src="resources/pic/3.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo04.jpg" alt="description" />
-				<img src="resources/pic/photo04.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/4.jpg" alt="description" />
+				<img src="resources/pic/4.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo05.jpg" alt="description" />
-				<img src="resources/pic/photo05.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/5.jpg" alt="description" />
+				<img src="resources/pic/5.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo01.jpg" alt="description" />
-				<img src="resources/pic/photo01.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/6.jpg" alt="description" />
+				<img src="resources/pic/6.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo02.jpg" alt="description" />
-				<img src="resources/pic/photo02.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/7.jpg" alt="description" />
+				<img src="resources/pic/7.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo03.jpg" alt="description" />
-				<img src="resources/pic/photo03.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/8.jpg" alt="description" />
+				<img src="resources/pic/8.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo04.jpg" alt="description" />
-				<img src="resources/pic/photo04.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/9.jpg" alt="description" />
+				<img src="resources/pic/9.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo05.jpg" alt="description" />
-				<img src="resources/pic/photo05.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/10.jpg" alt="description" />
+				<img src="resources/pic/10.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo01.jpg" alt="description" />
-				<img src="resources/pic/photo01.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/11.jpg" alt="description" />
+				<img src="resources/pic/11.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo02.jpg" alt="description" />
-				<img src="resources/pic/photo02.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/12.jpg" alt="description" />
+				<img src="resources/pic/12.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo03.jpg" alt="description" />
-				<img src="resources/pic/photo03.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/13.jpg" alt="description" />
+				<img src="resources/pic/13.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo04.jpg" alt="description" />
-				<img src="resources/pic/photo04.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/14.jpg" alt="description" />
+				<img src="resources/pic/14.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo05.jpg" alt="description" />
-				<img src="resources/pic/photo05.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/15.jpg" alt="description" />
+				<img src="resources/pic/15.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo01.jpg" alt="description" />
-				<img src="resources/pic/photo01.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/16.jpg" alt="description" />
+				<img src="resources/pic/16.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo02.jpg" alt="description" />
-				<img src="resources/pic/photo02.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/17.jpg" alt="description" />
+				<img src="resources/pic/17.jpg" alt="description" class="preview" /></a>
 			</li>
 			<li>
-				<a href="#"><img src="resources/pic/photo03.jpg" alt="description" />
-				<img src="resources/pic/photo03.jpg" alt="description" class="preview" /></a>
-			</li>
-			<li>
-				<a href="#"><img src="resources/pic/photo04.jpg" alt="description" />
-				<img src="resources/pic/photo04.jpg" alt="description" class="preview" /></a>
-			</li>
-			<li>
-				<a href="#"><img src="resources/pic/photo05.jpg" alt="description" />
-				<img src="resources/pic/photo05.jpg" alt="description" class="preview" /></a>
-			</li>
-			<li>
-				<a href="#"><img src="resources/pic/photo04.jpg" alt="description" />
-				<img src="resources/pic/photo04.jpg" alt="description" class="preview" /></a>
-			</li>
-			<li>
-				<a href="#"><img src="resources/pic/photo05.jpg" alt="description" />
-				<img src="resources/pic/photo05.jpg" alt="description" class="preview" /></a>
+				<a href="#"><img src="resources/pic/18.jpg" alt="description" />
+				<img src="resources/pic/18.jpg" alt="description" class="preview" /></a>
 			</li>
 			</ul>
 
